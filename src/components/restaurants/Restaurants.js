63b76509672b8287be0a56441242e9940b2a410a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 22:45:40 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+
   renderRestaurants = () =>
     this.props.restaurants.map(restaurant =>
       <Restaurant restaurant={restaurant} key={restaurant.id} delete={this.props.delete} />)
