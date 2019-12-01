37529803a499a4826708fe6ee0fb64f771620a67
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 22:49:35 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -8,8 +8,8 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput restaurantId={this.props.restaurant.id} addReview={this.props.addReview} />
+        <Reviews reviews={this.props.reviews} delete={this.props.delete} restaurant={this.props.restaurant} />
       </div>
     )
   }
