70be4af0d959387c9cfcb2cc3963f0f57078a330
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 22:48:27 +0000

    Automatically backed up by Learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
 
+import { connect } from 'react-redux';
+
 class RestaurantsContainer extends Component {
 
   render() {
