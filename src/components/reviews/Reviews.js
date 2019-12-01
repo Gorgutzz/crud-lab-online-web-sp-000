3706e4df4d796ed0e8c693bf28c2b020704b91f8
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 22:47:48 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -7,7 +7,7 @@ class Reviews extends Component {
     const reviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurant.id)
     return reviews.map(review => <Review review={review} key={review.id} delete={this.props.delete} />)
   }
-  
+
   render() {
     return (
       <ul>
